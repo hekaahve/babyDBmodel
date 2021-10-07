0919227d8a765dd675220f795b494e60cc171e76
hekaahve <heini.k.ahven@student.jyu.fi>
2021-10-07 22:19:06 +0300

    program added

--- EFdatabase/Program.cs
+++ EFdatabase/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Linq;
+using EFdatabase.Models;
 
 namespace EFdatabase
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new BabyContext())
+            {
+                // Note: This sample requires the database to be created before running.
+                Console.WriteLine($"Database path: {db.DbPath}.");
+                
+                // Create
+                Console.WriteLine("Inserting a new action");
+                db.Add(new Poo 
+                    {    
+                        Date="7/10/2021",  
+                        Time= "21:42", 
+                        Color="yellow", 
+                        Quality="good",
+                        Delivery="easy"
+                    });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Poo
+                    .OrderBy(b => b.Id)
+                    .First();
+                for (int i = 0; i<2; i++){
+                    Console.WriteLine(db.Poo);
+                }
+                
+
+                // Update
+                /*Console.WriteLine("Updating the blog and adding a pee");
+                blog.Url = "https://devpoos.microsoft.com/dotnet";
+                blog.Pee.Add(
+                    new Pee { Date = "7/10", Number =  "5"});
+                db.SaveChanges();
+                */
+                // Delete
+                /*
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+                */
+            }
         }
     }
 }
